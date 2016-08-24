ef32d57d66e51b97dfdb256cf55cb901bef8139d
zhou <634534398@qq.com>
2016-08-24 18:29:42 +0800

    初步成功,须微调

--- app/src/main/java/com/SupperHttp/android/MainActivity.java
+++ app/src/main/java/com/SupperHttp/android/MainActivity.java
@@ -2,6 +2,13 @@ package com.SupperHttp.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.SupperHttp.android.http.HttpCallBack;
+import com.SupperHttp.android.http.HttpClientAgent;
+import com.SupperHttp.android.http.JsonCallBack;
+
+import okhttp3.OkHttpClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        HttpClientAgent.getInstance().sendRequest(this,"http://www.jianshu.com/p/e740196225a4",false,String.class,new HttpCallBack<String>(){
 
+            @Override
+            public void onNetError() {
+                Toast.makeText(MainActivity.this,"网络错误",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onOtherError() {
+                Toast.makeText(MainActivity.this,"请求错误",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onSuccess(String s) {
+                Toast.makeText(MainActivity.this,"请求成功 String="+s,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
